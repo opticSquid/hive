a7c61cea9f18a8481ff32c9d1fd380820a98fa9c
Angelo Geant Gaviola <yomama@example.com>
2022-02-06 13:29:08 +0800

    Added the index route

--- Website/src/App.js
+++ Website/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Error from "./Pages/Error/Error";
+import Index from "./Pages/Index/Index"
 function App() {
     return (
         <Router>
             <Routes>
                 <Route exact path="/error" element={<Error errorMessage="404 not found" />}></Route>
+                <Route exact path="/" element={<Index />}></Route>
             </Routes>
         </Router>
     );
