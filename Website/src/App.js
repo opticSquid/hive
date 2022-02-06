af4640ab292dd0272cf4fa0cbc2f3d759eb7ec71
Angelo Geant Gaviola <yomama@example.com>
2022-02-06 11:44:52 +0800

    Added dynamic error message for the error route

--- Website/src/App.js
+++ Website/src/App.js
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Error from "./Pages/Error";
+import Error from "./Pages/Error/Error";
 function App() {
     return (
         <Router>
             <Routes>
-                <Route exact path="/error" element={<Error />}></Route>
+                <Route exact path="/error" element={<Error errorMessage="404 not found" />}></Route>
             </Routes>
         </Router>
     );
