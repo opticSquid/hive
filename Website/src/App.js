2203d29dee0d169c795647ea0596ea675b1f4b7c
Angelo Geant Gaviola <yomama@example.com>
2022-02-06 09:00:57 +0800

    Initialized the routes and mounted the Error route

--- Website/src/App.js
+++ Website/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import Error from "./Components/Error";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route exact path="/error" element={<Error />}></Route>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
