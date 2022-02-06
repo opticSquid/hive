259aba9bf4d751dc3e0366e2d03ca1546f86bd96
Angelo Geant Gaviola <yomama@example.com>
2022-02-06 13:01:20 +0800

    Changed "class" to "className"

--- Website/src/Pages/Error/Error.js
+++ Website/src/Pages/Error/Error.js
@@ -5,11 +5,11 @@ import "./Error.css";
 function Error(props) {
     return (
         <div id="error-page">
-            <div class="error-page">
+            <div className="error-page">
                 <img id="logo" src={Logo} alt="logo"></img>
                 <h1 id="header">Whoops!</h1>
-                <div class="content">
-                    <p class="error-message">{props.errorMessage}</p>
+                <div className="content">
+                    <p className="error-message">{props.errorMessage}</p>
                     <p>You'd have to go back!</p>
                     <button>Go back</button>
                 </div>
