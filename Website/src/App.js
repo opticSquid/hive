fc7a8b512508fbabe19fd027c35c0f2db3b5320f
Angelo Geant Gaviola <yomama@example.com>
2022-02-06 09:06:46 +0800

    Initialized the routing and added the error route

--- Website/src/App.js
+++ Website/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Error from "./Components/Error";
+import Error from "./Pages/Error";
 function App() {
     return (
         <Router>
