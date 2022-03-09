446537ed6ae9856d7b1eab59f5d25f098de30901
Tess-Vanta <tejaswimhtp@gmail.com>
2022-03-09 21:02:34 +0400

    Added profile page

--- Website/src/App.js
+++ Website/src/App.js
@@ -8,6 +8,7 @@ import Home from "./pages/Home/Home";
 import Content from "./pages/Content/Content";
 import Error from "./pages/Error/Error";
 import Analytics from "./pages/Analytics/Analytics";
+import Profile from "./pages/Profile/Profile";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/content" element={<Content />} />
         <Route path="/analytics" element={<Analytics />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="*" element={<Error errorMessage="404 not found" />} />
       </Routes>
     </Router>
